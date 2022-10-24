1ba664195b4cec21aa98aa53b2cee8e0bbdd1d64
UncleSamTech <samueloel3486@gmail.com>
2022-10-24 12:25:54 -0600

    failedtest

---
+++ app/src/test/java/CustomListTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.assertEquals;
+
+import com.example.cmput_301_lab8_demo.City;
+import com.example.cmput_301_lab8_demo.CustomList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class CustomListTest {
+    private CustomList list;
+    /**
+     * we create a CustomList object before running
+     */
+    @Before
+    public void createList(){
+        list = new CustomList(null,new ArrayList<City>());
+    }
+
+    @Test
+    public void addCityTest(){
+        int listSize = list.getCount();
+        list.addCity(new City("Burnaby", "BC"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
+
+}
